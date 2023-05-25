711e7c04ba302f863f300c4a0fd0b68d28f5f680
MatheusAlvesCavalcante <matheuscavalcantemt21@gmail.com>
2023-05-25 08:07:39 -0300

    Retomada de conteúdo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Pessoa adao = new Pessoa();
+                Pessoa eva = new Pessoa();
+
+                // definição do formato
+        adao.nome = "Adão";
+                adao.sobrenome = "Silva";
+
+                eva.nome = "Eva";
+                        eva.sobrenome="Silva";
+
+                        adao.falar();
+                        eva.falar();
+
+        // Criar mais objetos
+        Pessoa qualquer = new Pessoa();
+        //Declarar objeto
+        Pessoa rainha;
+        // Insraciação do obejeto
+        rainha = new Pessoa();
+        // Definindo forma de pessoa
+        rainha.nome = "Tina";
+        rainha.sobrenome= "Tunner";
+        // definição de comportamento
+        System.out.println(rainha.falar("alto"));
+
+
+
     }
 }
