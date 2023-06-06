80575b5b69dc8582e2237ae15a3e8cc173e646d0
MatheusAlvesCavalcante <matheuscavalcantemt21@gmail.com>
2023-06-06 08:13:13 -0300

    primeira classe

---
+++ Balonismo/src/Passagueiro.java
@@ -0,0 +1,12 @@
+public class Passagueiro {
+    String nome;
+    String termo_risco;
+    int CPF;
+    int RG;
+    int telefone;
+    int tel_emergencia;
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
