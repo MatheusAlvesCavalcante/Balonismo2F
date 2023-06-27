f11754dca70adf5ff6e6744eae5f64ec64d88518
Matheus <matheuscavalcantemt21@gmail.com>
2023-06-27 09:02:23 -0300

    Classe nova hull

--- Balonismo/src/Piloto.java
+++ Balonismo/src/Piloto.java
@@ -1,2 +1,23 @@
 public class Piloto {
+    String nome;
+    String CPF;
+    int numeroAnac;
+
+    String clima_viagem;
+    public Piloto(){
+
+    }
+
+    public Piloto(String nome, String CPF, int numeroAnac){
+        this.nome = "Matheus";
+        this.CPF = "12345678912";
+        this.numeroAnac= 001;
+
+    }
+    public Piloto(String clima_viagem){
+        this.clima_viagem = "bom";
+    }
 }
+
+
+
